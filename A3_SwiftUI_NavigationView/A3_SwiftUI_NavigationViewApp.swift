b0e50907ad199f667ba4357056acbe3b469d6658
k-yamaki <k.yamaki@hiroshimamm.com>
2021-08-19 14:00:47 +0900

    Initial Commit

---
+++ A3_SwiftUI_NavigationView/A3_SwiftUI_NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  A3_SwiftUI_NavigationViewApp.swift
+//  A3_SwiftUI_NavigationView
+//
+//  Created by keiji yamaki on 2021/08/19.
+//
+
+import SwiftUI
+
+@main
+struct A3_SwiftUI_NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
